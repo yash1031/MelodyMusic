720c7358241afb0f9d7b789414eee419c5b3f060
yash1031 <yash735131@gmail.com>
2025-03-02 00:21:37 +0530

    Design for All Album Categories

--- client/src/Components/MainScreen/AllContent/AllContent.js
+++ client/src/Components/MainScreen/AllContent/AllContent.js
@@ -8,9 +8,12 @@ const AllContent = () => {
   const [canScrollLeft, setCanScrollLeft] = useState(false);
   const [canScrollRight, setCanScrollRight] = useState(true);
 
+  const [sections, setSections]= useState(['Recently Played', 'Made For YASH GOYAL', 'Best Of Artists', 'Trending Music', 'Old Melodies', 'Best Of Albums']);
+
   const checkScrollPosition = () => {
       if (!scrollContainerRef.current) return;
       const { scrollLeft, scrollWidth, clientWidth } = scrollContainerRef.current;
+      console.log("scrollContainerRef.current ", scrollContainerRef.current);
       setCanScrollLeft(scrollLeft > 0);
       setCanScrollRight(scrollLeft < scrollWidth - clientWidth);
   };
@@ -24,23 +27,30 @@ const AllContent = () => {
 
   return (
     <div id="all-content">
-      This is AllContent  
-      <div className="section">
-        {canScrollLeft && <button className="scroll-btn left" onClick={() => scroll("left")}><i className="fa-solid fa-less-than"/></button>}
-            <div className="scroll-container" ref={scrollContainerRef} onScroll={checkScrollPosition}>
-                {arr.map((item, idx)=>{
-                  return  ( 
-                            <div key={idx} className="card" style={{}}>
-                              <div className="card-body">
-                                <h5 className="card-title">Card title. How is it?</h5>
-                                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                              </div>
-                            </div>
-                          )
-                  })} 
+      {sections.map((item, idx)=>{
+        return (
+          <>
+              <div><h1>{item}</h1></div>
+              <div className="section">
+              {canScrollLeft && <button className="scroll-btn left" onClick={() => scroll("left")}><i className="fa-solid fa-less-than"/></button>}
+                  <div className="scroll-container" ref={scrollContainerRef} onScroll={checkScrollPosition}>
+                      {arr.map((item, idx)=>{
+                        return  ( 
+                                  <div key={idx} className="card">
+                                    <div className="card-body">
+                                      <h5 className="card-title">Card title. How is it?</h5>
+                                      <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                    </div>
+                                  </div>
+                                )
+                        })} 
+                    </div>
+              {canScrollRight && <button className="scroll-btn right" onClick={() => scroll("right")}><i className="fa-solid fa-greater-than"/></button>}
               </div>
-        {canScrollRight && <button className="scroll-btn right" onClick={() => scroll("right")}><i className="fa-solid fa-greater-than"/></button>}
-      </div>
+          </>
+        )
+      }) }
+      
     </div>
   )
 }
